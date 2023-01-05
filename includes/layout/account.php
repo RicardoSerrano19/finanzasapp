ae93b29fda1363a19810eafc17dc652ed788f8c9
Ricardo Serrano <ricardo.ss@live.com>
2023-01-05 17:07:15 -0600

    feat: add account layout

---
+++ includes/layout/account.php
@@ -0,0 +1,7 @@
+<section class="account">
+    <h3>Cuentas en pesos</h3>
+    <div class="account-balance">
+        <p class="account-balance-money">$490<sup>.58</sup></p>
+        <p>Saldo disponible</p>
+    </div>
+</section>
