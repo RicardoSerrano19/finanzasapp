29d1aed266301ecfa23a92c9e3873c5efa60bbcd
Ricardo Serrano <ricardo.ss@live.com>
2023-01-05 17:07:01 -0600

    feat: add navigation layout

---
+++ includes/layout/navigation.php
@@ -0,0 +1,30 @@
+<header>
+    <nav class="navigation">
+        <ul class="navigation-list">
+            <li class="navigation-list-item">
+                <a href="#" class="navigation-list-item-link">
+                    <i class="fas fa-home"></i>
+                    Principal
+                </a>
+            </li>
+            <li class="navigation-list-item">
+                <a href="#" class="navigation-list-item-link">
+                    <i class="fas fa-rocket"></i>
+                    Ingresos
+                </a>
+            </li>
+            <li class="navigation-list-item">
+                <a href="#" class="navigation-list-item-link">
+                    <i class="fas fa-angle-double-down"></i>
+                    Egresos
+                </a>
+            </li>
+            <li class="navigation-list-item">
+                <a href="#" class="navigation-list-item-link">
+                    <i class="fas fa-file-download"></i>
+                    Reportes
+                </a>
+            </li>
+        </ul>
+    </nav>
+</header>
