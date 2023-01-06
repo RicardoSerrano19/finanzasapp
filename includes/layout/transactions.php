af62477a4516b0e521e9068f92488b986c15fa1c
Ricardo Serrano <ricardo.ss@live.com>
2023-01-06 13:15:55 -0600

    feat: add transactions layout

---
+++ includes/layout/transactions.php
@@ -0,0 +1,64 @@
+<section class="transactions">
+    <p class="transactions-title">Ultimas transacciones</p>
+    <div class="transaction">
+        <div class="transaction_date">
+            20 dic. 2022
+        </div>
+        <div class="transactions_table">
+            
+            <div class="transactions_table-icon">
+                <i class="fas fa-wallet i"></i>
+            </div>
+            <div class="transactions_table-info">
+                <p class="transactions_table-info-reason">
+                    Ingreso de nomina
+                </p>
+                <p class="transactions_table-info-time">
+                    11:00pm
+                </p>
+            </div>
+            <div class="transactions_table-amount">
+                $1600<sup>.58</sup>
+            </div>
+        </div>
+    </div>
+    <div class="transaction">
+        <div class="transaction_date">
+            20 dic. 2022
+        </div>
+        <div class="transactions_table">
+            
+            <div class="transactions_table-icon">
+                <i class="fas fa-wrench e"></i>
+            </div>
+            <div class="transactions_table-info">
+                <p class="transactions_table-info-reason">
+                    Total play
+                </p>
+                <p class="transactions_table-info-time">
+                    11:00pm
+                </p>
+            </div>
+            <div class="transactions_table-amount">
+                - $1600<sup>.58</sup>
+            </div>
+        </div>
+        <div class="transactions_table">
+            
+            <div class="transactions_table-icon">
+                <i class="fas fa-wallet i"></i>
+            </div>
+            <div class="transactions_table-info">
+                <p class="transactions_table-info-reason">
+                    Ingreso de nomina
+                </p>
+                <p class="transactions_table-info-time">
+                    11:00pm
+                </p>
+            </div>
+            <div class="transactions_table-amount">
+                + $1600<sup>.58</sup>
+            </div>
+        </div>
+    </div>
+</section>
