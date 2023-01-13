3b0ef26a5b5fda2d84ac8de5665448929758934e
Ricardo Serrano <ricardo.ss@live.com>
2023-01-13 11:27:34 -0600

    feat: add incomes to incomes page

---
+++ incomes.php
@@ -0,0 +1,14 @@
+<?php
+       include 'includes/layout/header.php';
+       include 'includes/layout/navigation.php';
+?>
+
+<main>
+       <h1>Ingresos</h1>
+       <?php
+              include './includes/layout/form.php';
+              include './includes/layout/transactions.php';
+       ?>
+</main>
+
+<script src="./js/incomes.js"></script>
