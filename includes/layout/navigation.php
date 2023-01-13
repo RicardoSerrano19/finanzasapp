7638833e3d2a2e43ec6b087177dd33bbd46d1647
Ricardo Serrano <ricardo.ss@live.com>
2023-01-12 19:50:03 -0600

    feat: add router for navigation elements

--- includes/layout/navigation.php
+++ includes/layout/navigation.php
@@ -2,13 +2,13 @@
     <nav class="navigation">
         <ul class="navigation-list">
             <li class="navigation-list-item">
-                <a href="#" class="navigation-list-item-link">
+                <a href="./index.php" class="navigation-list-item-link">
                     <i class="fas fa-home"></i>
                     Principal
                 </a>
             </li>
             <li class="navigation-list-item">
-                <a href="#" class="navigation-list-item-link">
+                <a href="./incomes.php" class="navigation-list-item-link">
                     <i class="fas fa-rocket"></i>
                     Ingresos
                 </a>
